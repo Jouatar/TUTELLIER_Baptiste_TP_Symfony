db63cb62a2f70418490bc6fb686cbc8cb0f74562
jouatar <baptistetutellierpro@gmail.com>
2021-11-22 14:34:03 +0000

    fin cours 2 : Tableau article

--- exoSymfony/src/Controller/ArticleController.php
+++ exoSymfony/src/Controller/ArticleController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class ArticleController extends AbstractController
 {
     /**
      * @Route("/article", name="article.index")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+        dd($articles);
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'listArticles' => 'articles',
         ]);
     }
 }
